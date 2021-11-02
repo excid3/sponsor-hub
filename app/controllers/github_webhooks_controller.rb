8d14fe20d7f22e5c9998108e1cba7f13b1b682eb
Richard Logwood <richard.logwood.com>
2021-11-02 18:18:10 -0400

    pretty print github webhook json to stdout for investigation

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,9 +1,17 @@
+require 'neatjson'
+
 class GithubWebhooksController < ApplicationController
   protect_from_forgery with: :null_session, only: [:sponsored]
   #protect_from_forgery with: :null_session
 
   def sponsored
     puts "\n***\nsponsered: params\n#{params}\n***\n"
+
+    puts "\n\n*** json ***\n"
+    payload = JSON.parse(params['payload'])
+
+    puts JSON.neat_generate(payload, sort: true, wrap: 40, aligned: true, around_colon: 1)
+
     render nothing: true, layout: false, status: :ok
   end
 end
